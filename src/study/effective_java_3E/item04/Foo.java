ab6fa44128fcd84bb1ca6833e61390aee4fef1b2
bziaco <bjh2501@gmail.com>
2019-05-22 01:04:03 +0900

    item04 수정

---
+++ src/study/effective_java_3E/item04/Foo.java
@@ -0,0 +1,19 @@
+package study.effective_java_3E.item04;
+
+public class Foo {
+
+	public static void main(String[] args) {
+		
+		//private 생성자를 만들기 전 객체가 만들어진다.
+		//FooBehavior객체에 private 생성자를 생성하면 객체가 안만들어진다.
+		FooBank 저금 = new FooBank();
+		
+		
+		int 내재산 = 10000000;
+		int 월급 = 2000000;
+		FooBank.plus(내재산, 월급);
+		
+
+	}
+
+}
---
+++ src/study/effective_java_3E/item04/FooBank.java
@@ -0,0 +1,11 @@
+package study.effective_java_3E.item04;
+
+public class FooBank {
+	
+	private FooBank() {}
+	
+	static int plus(int 내재산, int 월급) {
+		내재산 =+ 월급;
+		return 내재산;
+	}
+}
